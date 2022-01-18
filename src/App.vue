e15054dcbb88f41de1c53dbd0d9c8c16d33959e9
Laurence <laurencejuanwalton@gmail.com>
2022-01-18 16:43:08 +0200

    add: RISK-662 Create stock Vue app using CLI

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1> {{msg}} </h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      msg: "Dune > Star Wars"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2cm;
+}
+</style>
