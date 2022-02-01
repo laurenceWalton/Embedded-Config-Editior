0f44a77f1ce0af81d05d2d23e54a61fa2a2384de
Laurence <laurencejuanwalton@gmail.com>
2022-02-01 15:32:06 +0200

    update RISK-666 Change reader to single component

--- src/TextReader.vue
+++ src/TextReader.vue
@@ -1,21 +1,37 @@
 <template>
   <label class="text-reader">
-    Upload File
-  <input type="file" @change="loadTextFromFile" />
+    {{ name }}
+    <input type="file" @change="loadTextFromFile" />
   </label>
 </template>
 
 <script>
 export default {
-  methods: { 
+  props: ["name"],
+  methods: {
     loadTextFromFile(ev) {
       const file = ev.target.files[0];
       const reader = new FileReader();
 
-      reader.onload = e => this.$emit("load", e.target.result);
+      reader.onload = (e) => this.$emit("load", e.target.result);
       reader.readAsText(file);
-      
-    }
-  }  
+    },
+  },
 };
 </script>
+
+<style scoped>
+.text-reader {
+  border: 2px solid black;
+  border-radius: 5px;
+  padding: 8px 12px;
+  cursor: pointer;
+}
+.text-reader input {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  opacity: 0;
+}
+</style>
